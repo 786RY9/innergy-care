2d956927a1a76282f46da3ce87d43770b59a3b10
ryaseen <ryaseen.bscs22seecs@seecs.edu.pk>
2026-05-06 19:52:56 +0500

    fix: sync tina-lock.json and regenerate client with correct Tina Cloud URL

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/ry/Pictures/innergy_care/innergy-care/tina/__generated__/.cache/1778076576556', url: 'https://content.tinajs.io/2.3/content/dafe0207-2b1b-4cc7-9c6b-fbff72b6740b/github/main', token: 'b6c4b3d9259a89eb6904997b27787058bb288be4', queries,  });
+export const client = createClient({ cacheDir: '/home/ry/Pictures/innergy_care/innergy-care/tina/__generated__/.cache/1778079038992', url: 'https://content.tinajs.io/2.3/content/dafe0207-2b1b-4cc7-9c6b-fbff72b6740b/github/main', token: 'b6c4b3d9259a89eb6904997b27787058bb288be4', queries,  });
 export default client;
   
